584c4bd622903c54580172aadc4ac38d6708f741
mylukhanush <mylukhanush103@gmail.com>
2025-04-16 17:53:22 +0530

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -1,46 +1,46 @@
 
-#  # set data type
-# s={0,1,2,3,1,2,3,4,2,3,4,23}
-# # print(s)
-# s.add(6)
-# # print(s)
-# s.update(["ps"])
-# # print(s)
-# s.remove(2)
-# # print(s)
+ # set data type
+s={0,1,2,3,1,2,3,4,2,3,4,23}
+print(s)
+s.add(6)
+# print(s)
+s.update(["ps"])
+print(s)
+s.remove(2)
+print(s)
 
 # dictionary type
-# a={1:"Bunny",2:"harsh"}
-# print(a)
-# del(a[1])
-# print(a)
-# for i,j in a.items():
-#     print(i,j)
-# for key in a.keys():
-#     print(key)
-# for value in a.values():
-#     print(values)
+a={1:"Bunny",2:"harsh"}
+print(a)
+del(a[1])
+print(a)
+for i,j in a.items():
+    print(i,j)
+for key in a.keys():
+    print(key)
+for value in a.values():
+    print(values)
 
-#Tuples
+# Tuples
 # syntax to create tuple "()"
 # tuple properties
 # mutable ,immutable
 # indexing
-# k="Achieve"
-# print(k)   #THIS LOGIC DEFINES.......
-# print(type(k))
+k="Achieve"
+print(k)   #THIS LOGIC DEFINES.......
+print(type(k))
 # indexing
-# index values always starts from zero
+# index values always starts from zero/
 # 0    1    2    3    4    5    6      positive index
 # A    c    h    i    e    v    e
 # -7   -6   -5   -4   -3   -2   -1     negative index
-# print(k[3])
-# print(k*3)
-# print(k[3]*3)
-# print(len(k))
-#SLICING
-# print(k[0:3])
-# print(k[3:])
-# print(k[:4])
-# print(k[-3:-1:])
-# print(k[0:6:2])
+print(k[3])
+print(k*3)
+print(k[3]*3)
+print(len(k))
+# SLICING
+print(k[0:3])
+print(k[3:])
+print(k[:4])
+print(k[-3:-1:])
+print(k[0:6:2])
